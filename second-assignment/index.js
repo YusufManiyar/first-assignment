cc2e25e6df42edeedaa2927f580073c13f2d62ac
yusufmaniyar <yusufmaniyar108@gmail.com>
2023-12-06 17:06:57 +0530

    second assignment

---
+++ second-assignment/index.js
@@ -0,0 +1,8 @@
+let headTitle = document.getElementById('main-header')
+
+headTitle.style.borderBottom = 'solid 3px black'
+let text = document.getElementsByClassName('title')
+
+text[0].style.fontWeigth = 'bold'
+text[0].style.color = 'green'
+console.log(text[0])
