aeff24f56f685b51a3204048ea1874c1de60eb5c
yusufmaniyar <yusufmaniyar108@gmail.com>
2023-12-06 20:58:47 +0530

    add color to second item and hide third item by using queryselector

--- second-assignment/index.js
+++ second-assignment/index.js
@@ -1,13 +1,19 @@
-let headTitle = document.getElementById('main-header')
+// let headTitle = document.getElementById('main-header')
 
-headTitle.style.borderBottom = 'solid 3px black'
-let text = document.getElementsByClassName('title')
+// headTitle.style.borderBottom = 'solid 3px black'
+// let text = document.getElementsByClassName('title')
 
-text[0].style.fontWeigth = 'bold'
-text[0].style.color = 'green'
+// text[0].style.fontWeigth = 'bold'
+// text[0].style.color = 'green'
 
-let item = document.getElementsByTagName('li')
-item[2].style.backgroundColor = 'green'
-for(let i = 0; i < item.length; i++){
-    item[i].style.fontStyle = 'bold'
-}
+// let item = document.getElementsByTagName('li')
+// item[2].style.backgroundColor = 'green'
+// for(let i = 0; i < item.length; i++){
+//     item[i].style.fontStyle = 'bold'
+// }
+
+let secondItem = document.querySelector('.list-group-item:nth-child(2)')
+ 
+ secondItem.style.backgroundColor = 'green'
+ let thirdItem = document.querySelector('.list-group-item:nth-child(3)')
+ thirdItem.style.visibility = 'hidden'
