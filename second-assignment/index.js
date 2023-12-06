99511b01ec62cdba64403b28e4d9fd6b89816c12
yusufmaniyar <yusufmaniyar108@gmail.com>
2023-12-06 21:25:50 +0530

    add color for second item list to all and change backgroundColor of odd number list by using queryselectorAll

--- second-assignment/index.js
+++ second-assignment/index.js
@@ -12,8 +12,13 @@
 //     item[i].style.fontStyle = 'bold'
 // }
 
-let secondItem = document.querySelector('.list-group-item:nth-child(2)')
- 
- secondItem.style.backgroundColor = 'green'
- let thirdItem = document.querySelector('.list-group-item:nth-child(3)')
- thirdItem.style.visibility = 'hidden'
+let oddItem = document.querySelectorAll('.list-group-item:nth-child(odd)')
+for(let i = 0; i < oddItem.length; i++){
+    oddItem[i].style.backgroundColor = 'green'
+}
+
+let secondItem = document.querySelectorAll('li')
+console.log(secondItem)
+for(let i = 1; i < secondItem.length; i++){
+    secondItem[i].style.color = 'green'
+}
